7bc0f1bc35f3706f50fba55ff9b5fe901c1185ed
gfox974 <nosmas.ju@gmail.com>
2019-11-25 15:55:58 +0100

    arch

--- monprojet/TourOfHeroes/src/app/app-routing.module.ts
+++ monprojet/TourOfHeroes/src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
+// Ici on gere les redirect de route, et par defaut -> go dash
+const routes: Routes = [
+{ 
+  path: '',
+  redirectTo: '/dashboard',
+  pathMatch: 'full'
+},  
+{ 
+  path: 'heroes',
+  component: HeroesComponent
+},
+{ 
+  path: 'dashboard',
+  component: DashboardComponent
+},
+{ 
+  path: 'detail/:id', // On utilise un placeholder comme en php, où :id corresponds a une valeur
+  component: HeroDetailComponent
+},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- monprojet/TourOfHeroes/src/app/heroes/heroes.component.ts
+++ monprojet/TourOfHeroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'; // import de la classe Hero
-import { HEROES } from '../mock-heroes'; // Import de la constante array d'objets Heroes
+//import { HEROES } from '../mock-heroes'; // Import de la constante array d'objets Heroes
+import { HeroService } from '../hero.service'; // Import du service HeroService
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +11,40 @@ import { HEROES } from '../mock-heroes'; // Import de la constante array d'objet
 
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES; // On affecte heroes = HEROES ( l'array importée )
-  selectedHero: Hero; // On affecte selectedHero en tant que Hero (la classe)
+  //heroes = HEROES; // On affecte heroes = HEROES ( l'array importée )
+  heroes: Hero[]; // On affecte heroes comme etant les instances de Hero dans l'espace appli
+  //selectedHero: Hero; // On affecte selectedHero en tant que Hero (la classe)
 
-  constructor() { }
+  constructor(private heroService: HeroService) { } // On injecte le HeroService en definissant un constructeur privé
+//  constructor() { }
 
-  ngOnInit() {
+  //onSelect(hero: Hero): void { // Event trigger sur le on select (click) -> on set la proprieté selectedHero a la valeur de l'objet instancié du click (si l'objet selectionné a la classe hero)
+  //  this.selectedHero = hero;
+  //}
+  //getHeroes(): void { // on declare la fonction pour recuperer les heroes de ce composant
+  //  this.heroes = this.heroService.getHeroes();
+  //}
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero): void { // Event trigger sur le on select (click) -> on set la proprieté selectedHero a la valeur de l'objet instancié du click (si l'objet selectionné a la classe hero)
-    this.selectedHero = hero;
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
+  ngOnInit() { // Là, on dit au composant qu'a son init, il doit lancer sa fonction getheroes
+    this.getHeroes();
+  }
+  
 }
