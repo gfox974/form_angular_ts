ee5eb7dcb5e3e5887974456c894ca7099b6064c7
gfox974 <nosmas.ju@gmail.com>
2019-11-25 17:29:12 +0100

    arch

--- monprojet/TourOfHeroes/src/app/app-routing.module.ts
+++ monprojet/TourOfHeroes/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { ContactComponent }   from './contact/contact.component';
+import { CompteurComponent }   from './compteur/compteur.component';
 
 // Ici on gere les redirect de route, et par defaut -> go dash
 const routes: Routes = [
@@ -22,6 +24,14 @@ const routes: Routes = [
 { 
   path: 'detail/:id', // On utilise un placeholder comme en php, où :id corresponds a une valeur
   component: HeroDetailComponent
+},
+{ 
+  path: 'contact',
+  component: ContactComponent
+},
+{ 
+  path: 'compteur',
+  component: CompteurComponent
 },
 
 ];
--- monprojet/TourOfHeroes/src/app/app.module.ts
+++ monprojet/TourOfHeroes/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { HttpClientModule }    from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { ContactComponent } from './contact/contact.component';
+import { CompteurComponent } from './compteur/compteur.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    ContactComponent,
+    CompteurComponent
   ],
   imports: [
     BrowserModule,
